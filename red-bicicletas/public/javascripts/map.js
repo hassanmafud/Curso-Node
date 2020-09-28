70bd28122cefaef30dc5aee0d7eacf78aa7a67d3
Hassan Mafud <hassanmafud97@gmail.com>
2020-09-28 14:22:45 -0300

    paguina principal completa

---
+++ red-bicicletas/public/javascripts/map.js
@@ -0,0 +1,16 @@
+var mymap = L.map('main_map').setView([-31.4488265,-64.1847824,17], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+    }).addTo(mymap);
+    
+L.marker([-31.4485668,-64.1849058,19]).addTo(mymap);
+L.marker([-31.4423305,-64.1946111,17]).addTo(mymap);
+
+    
